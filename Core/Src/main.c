863c46ca1b77cf4554b675fe9febb447c932f0dd
roryschram <66696753+roryschram@users.noreply.github.com>
2023-09-27 11:51:09 +0200

    Fixed Lora Mdule with reset low issue

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -448,6 +448,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
